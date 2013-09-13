e025485eb6550c7a547cbfc39770b3e6d21f4a3c
Lk Xf <elveestei@gmail.com>
2013-09-13 19:49:54 +0200

    Completed CLHeading hijack to support readwrite properties

--- AstralProjection/Core/CLHeading+AstralProjection.m
+++ AstralProjection/Core/CLHeading+AstralProjection.m
@@ -18,7 +18,31 @@ static const char* kAP_CLHeading_xKey = "ap_x";
 static const char* kAP_CLHeading_yKey = "ap_y";
 static const char* kAP_CLHeading_zKey = "ap_z";
 
+static IMP CLHeading_magneticHeading = NULL;
+static IMP CLHeading_trueHeading = NULL;
+static IMP CLHeading_headingAccuracy = NULL;
 static IMP CLHeading_timestamp = NULL;
+static IMP CLHeading_x = NULL;
+static IMP CLHeading_y = NULL;
+static IMP CLHeading_z = NULL;
+
+
+#define AP_CLHEADING_PROXY(methodName,methodType)								\
+methodType AP_CLHeading_##methodName(id aSelf, SEL aCmd)						\
+{																				\
+	id obj = objc_getAssociatedObject(aSelf, kAP_CLHeading_##methodName##Key);	\
+	if (obj) { return (methodType)[(NSNumber*)obj doubleValue]; }				\
+	return ((methodType(*)(id,SEL))CLHeading_##methodName)(aSelf, aCmd);		\
+}
+
+AP_CLHEADING_PROXY(magneticHeading, CLLocationDirection);
+AP_CLHEADING_PROXY(trueHeading, CLLocationDirection);
+AP_CLHEADING_PROXY(headingAccuracy, CLLocationDirection);
+AP_CLHEADING_PROXY(x, CLHeadingComponentValue);
+AP_CLHEADING_PROXY(y, CLHeadingComponentValue);
+AP_CLHEADING_PROXY(z, CLHeadingComponentValue);
+
+#undef AP_CLHEADING_PROXY
 
 
 id AP_CLHeading_timestamp(id aSelf, SEL aCmd)
@@ -33,8 +57,21 @@ id AP_CLHeading_timestamp(id aSelf, SEL aCmd)
 
 + (void)load
 {
-	Method m = class_getInstanceMethod([self class], @selector(timestamp));
-	CLHeading_timestamp = method_setImplementation(m, (IMP)AP_CLHeading_timestamp);
+	Method m;
+	
+#define AP_CLHEADING_SWIZZLE(methodName)									\
+	m = class_getInstanceMethod([self class], @selector(methodName));		\
+	CLHeading_##methodName = method_setImplementation(m, (IMP)AP_CLHeading_##methodName);
+	
+	AP_CLHEADING_SWIZZLE(magneticHeading);
+	AP_CLHEADING_SWIZZLE(trueHeading);
+	AP_CLHEADING_SWIZZLE(headingAccuracy);
+	AP_CLHEADING_SWIZZLE(timestamp);
+	AP_CLHEADING_SWIZZLE(x);
+	AP_CLHEADING_SWIZZLE(y);
+	AP_CLHEADING_SWIZZLE(z);
+
+#undef AP_CLHEADING_SWIZZLE
 }
 
 
@@ -47,8 +84,17 @@ id AP_CLHeading_timestamp(id aSelf, SEL aCmd)
 									   z:(CLHeadingComponentValue)aZ
 {
 	CLHeading* heading = [[CLHeading alloc] init];
+
+	objc_setAssociatedObject(heading, kAP_CLHeading_magneticHeadingKey, @(aMagneticHeading), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	objc_setAssociatedObject(heading, kAP_CLHeading_trueHeadingKey, @(aTrueHeading), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	objc_setAssociatedObject(heading, kAP_CLHeading_headingAccuracyKey, @(aAccuracy), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	objc_setAssociatedObject(heading, kAP_CLHeading_xKey, @(aX), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	objc_setAssociatedObject(heading, kAP_CLHeading_yKey, @(aY), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	objc_setAssociatedObject(heading, kAP_CLHeading_zKey, @(aZ), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 	
-	return heading;
+	heading.timestamp = aTimestamp;
+
+	return [heading autorelease];
 }
 
 
